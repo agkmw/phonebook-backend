7cbf8ffe6f999551dfce64fcc8363ab5a46a899f
aungkhantmaw <185657819+agkmw@users.noreply.github.com>
2024-11-22 08:24:55 +0630

    fix: displaying wrong entries when an entry is deleted

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.get("/api/persons/:id", (req, res) => {
 
 app.delete("/api/persons/:id", (req, res) => {
   const id = req.params.id;
-  persons = persons.filter((p) => p.id === id);
+  persons = persons.filter((p) => p.id !== id);
 
   res.status(204).end();
 });
